8f887e7784acd350688bc796f47c8cc26287d3b5
mora <molra@gmail.com>
2020-12-14 14:09:19 +0900

    로그인 체크

--- src/main/java/kr/co/chase/ncms/common/AuthenticInterceptor.java
+++ src/main/java/kr/co/chase/ncms/common/AuthenticInterceptor.java
@@ -6,6 +6,8 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.ModelAndViewDefiningException;
 import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
 
 import kr.co.chase.ncms.common.util.StringUtil;
@@ -17,6 +19,7 @@ import kr.co.chase.ncms.common.util.StringUtil;
 public class AuthenticInterceptor extends HandlerInterceptorAdapter{
 	@Override
 	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+		String mainUrl = "/index.jsp";
 		String requestURI = request.getRequestURI();	//요청 URI
 		int requestPORT = request.getServerPort();		//요청 PORT
 		String requestHOST = request.getServerName();
@@ -35,7 +38,8 @@ public class AuthenticInterceptor extends HandlerInterceptorAdapter{
 			HashMap<String, Object> usrInfo = (HashMap<String, Object>)session.getAttribute(ConstantObject.LOGIN_SESSEION_INFO);
 
 			if(usrInfo == null || StringUtil.nvl(usrInfo.get("USR_ID").toString(), "") == "") {
-//				isPermittedURL = false;
+				ModelAndView modelAndView = new ModelAndView("redirect:" + mainUrl);
+				throw new ModelAndViewDefiningException(modelAndView);
 			}else {
 				request.setAttribute("LoginUserId", StringUtil.nvl(usrInfo.get("USR_ID"), ""));
 				request.setAttribute("LoginUserNm", StringUtil.nvl(usrInfo.get("USR_NM"), ""));
